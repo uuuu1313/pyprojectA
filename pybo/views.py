269682734fe0456143e286f0787cfc0e465d22c4
uuuu13 <a51903153@gmail.com>
2024-05-06 17:20:16 +0900

    데이터저장(answer)

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question # tb 추가
 
 # Create your views here.
@@ -12,6 +13,13 @@ def index(request) :
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id) # question_id가 없으면 404에러 리턴
-    context = {'question_' : question} # .html question_은 question 값이 대입됨
+    context = {'question' : question} # .html question은 question 값이 대입됨
     return render(request, 'pybo/question_detail.html', context)
 
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # Question과 Answer는 ForeignKey로 연결되어 question에서 answer를 참조 가능 (question.answer_set.create~)
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now()) #위와 동일
+    # 위 코드를 사용시 answer.save() 세이브를 해야함
+    return redirect('pybo:detail', question_id=question.id)
